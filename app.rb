59fc55bd8699ab1b04e548cbb227e5a4096df88f
github username <github email address>
2018-04-05 09:25:41 -0400

    done

--- app.rb
+++ app.rb
@@ -2,4 +2,22 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
+  get '/' do
+    erb :new
+  end
+
+post '/student' do
+  @student = Student.new(params[:student])
+
+params[:student][:courses].each do |details|
+  Course.new(details)
+end
+# Then we iterate over `params[:student][:courses]`, which is an array containing a series of hashes that each store individual course
+
+@courses = Course.all
+
+erb :student
+end
+
+
 end
